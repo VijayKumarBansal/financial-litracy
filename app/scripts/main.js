a15ea3d58d108570469f5671be2117e973f764b0
VijayKumarBansal <vijaybansal15708@gmail.com>
2019-05-28 11:16:31 +0530

    Add files via upload

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,3 +7,45 @@ console.log('\'Allo \'Allo!');
 // Uncomment to enable Bootstrap popovers
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
+
+
+(function ($) {
+    "use strict";
+  
+    // Mobile Navigation
+    if ($('.main-nav').length) {
+      var $mobile_nav = $('.main-nav').clone().prop({
+        class: 'mobile-nav d-lg-none'
+      });
+      $('body').append($mobile_nav);
+      $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="fa fa-bars"></i></button>');
+      $('body').append('<div class="mobile-nav-overly"></div>');
+  
+      $(document).on('click', '.mobile-nav-toggle', function(e) {
+        $('body').toggleClass('mobile-nav-active');
+        $('.mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+        $('.mobile-nav-overly').toggle();
+      });
+      alert(12345);
+      $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
+        e.preventDefault();
+        $(this).next().slideToggle(300);
+        $(this).parent().toggleClass('active');
+      });
+  
+      $(document).click(function(e) {
+        var container = $(".mobile-nav, .mobile-nav-toggle");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+          if ($('body').hasClass('mobile-nav-active')) {
+            $('body').removeClass('mobile-nav-active');
+            $('.mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+            $('.mobile-nav-overly').fadeOut();
+          }
+        }
+      });
+    } else if ($(".mobile-nav, .mobile-nav-toggle").length) {
+      $(".mobile-nav, .mobile-nav-toggle").hide();
+    }
+  
+  })(jQuery);
+  
